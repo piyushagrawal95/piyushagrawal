32b96a5941fcb68acb801466fddae9b43943a41e
Piyush Agrawal <piyushagrawal300504@gmail.com>
2024-07-12 00:08:49 -0700

    Today question

---
+++ three_consecutive_odds.java
@@ -0,0 +1,21 @@
+package piyushagrawal;
+
+public class three_consecutive_odds {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2!=0){
+                count++;
+                if(count==3){
+                    return true;
+                }
+            }
+            else{
+                count=0;
+            }
+        }
+        return false;
+        
+    }
+    
+}
